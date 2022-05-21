1fd921f172edd5d684f4bd1c572cef4c709b149a
Heechan Moon <59304977+mun9769@users.noreply.github.com>
2022-05-21 11:40:43 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+
+
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    cout << a - b<<endl;
+    cout << a * b<<endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+}
