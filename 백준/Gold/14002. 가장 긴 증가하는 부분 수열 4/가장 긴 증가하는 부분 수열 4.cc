6ad877673d73be26e9720bafaff6e7ab12e7e4ea
Moon <59304977+mun9769@users.noreply.github.com>
2023-06-14 23:37:58 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, A[1001], len;
+int index, tmp;
+int dp[1001];
+vector<int> v;
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> A[i];
+		len = 0;
+
+		for (int j = 1; j < i; j++)
+		{
+			if (A[j] < A[i])
+				len = max(dp[j], len);
+		}
+		dp[i] = len + 1;
+		
+		// 최대 길이와 최대 값 저장
+		if (tmp < len + 1)
+		{
+			tmp = len + 1; // 최대 길이
+			index = i; // 최대 값의 인덱스
+		}
+	}
+
+	for (int i = index; i >= 1; i--)
+	{
+		if (dp[i] == tmp)
+		{
+			v.push_back(A[i]);
+			tmp--;
+		}
+	}
+
+	int size = v.size();
+	cout << size << '\n'; 
+	for (int i = 0; i < size; i++)
+	{
+		cout << v.back() << " ";
+		v.pop_back();
+	}
+}
