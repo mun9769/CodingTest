0ecae73c33995d0de4b910e0251bfd5d9332411c
Moon <59304977+mun9769@users.noreply.github.com>
2023-06-20 00:03:59 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define init cin.tie(0)->ios_base::sync_with_stdio(0);
+#define ii pair<int, int>
+#define INF 987654321
+
+int main(){
+  string s;
+  int i;
+  cin >> s;
+  cin >> i;
+  cout << s[i-1];
+}
