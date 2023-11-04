366987d6fa9b563cda38cb0c2d253c49c0a38e02
Moon <59304977+mun9769@users.noreply.github.com>
2023-11-04 22:47:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--> 0) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
