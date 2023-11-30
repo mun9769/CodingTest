bd743b53842b59f526c91416598de6940894289d
Moon <59304977+mun9769@users.noreply.github.com>
2023-11-30 15:02:06 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 3.11 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -6,11 +6,11 @@ int land[504][505];
 bool visited[505][505];
 int n, m;
 
-int oilId = 1;
 const int dx[] = {1, 0 ,-1, 0};
 const int dy[] = {0 ,-1, 0 ,1};
 
-ii oil[505][505];
+int mn, mx;
+int store[505];
 
 bool canGo(int nx, int ny) {
     if(nx < 1 || ny < 1 || nx > n || ny > m) return false;
@@ -19,57 +19,24 @@ bool canGo(int nx, int ny) {
     return true;
 }
 
-void bfs(int u, int v) {
-    if(visited[u][v]) return;
-    if(land[u][v] == 0) return;
-    
-    queue<ii> q;
-    q.push({u, v});
-    visited[u][v] = true;
-    vector<ii> location;
-    
-    while(q.size()) {
-        auto [x, y] = q.front();
-        q.pop();
-        location.push_back({x, y});
-        
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(canGo(nx, ny)) {
-                visited[nx][ny] = true;
-                q.push({nx, ny});
-            }
-        }
-    }
-    
-    int store = location.size();
-    for(auto [x, y]: location) {
-        oil[x][y] = {oilId, store};
-    }
-    oilId++;
-    
-}
-int ans;
 
-void drill() {
-    for(int j=1; j<=m; j++){
-        vector<int> drilledOil;
-        int sum = 0;
-        
-        for(int i=1; i<=n; i++){
-            int id = oil[i][j].first;
-            int store = oil[i][j].second;
-            
-            if(find(drilledOil.begin(), drilledOil.end(), id) == drilledOil.end()) {
-                sum += store;
-                drilledOil.push_back(id);
-            }
-        }
-        ans = max(ans, sum);
-        
+int drill(int x, int y){
+    if(canGo(x, y) == false) return 0;
+    visited[x][y] = true;
+    mn = min(mn, y);
+    mx = max(mx, y);
+    
+    int sum = 0;
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        sum += drill(nx, ny);
     }
+    
+    return sum + 1;
 }
+
+int ans;
 
 int solution(vector<vector<int>> _land) {
     int answer = 0;
@@ -80,13 +47,20 @@ int solution(vector<vector<int>> _land) {
         	land[i+1][j+1] = _land[i][j];
         }
     }
+    
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            bfs(i, j);
+    	for(int j=1; j<=m; j++) {
+            mn = j;
+            mx = j;
+            int oils = drill(i, j);
+            
+            for(int k = mn; k<=mx; k++) store[k] += oils;
         }
     }
-    drill();
-    answer = ans;
+    
+    for(int i=1; i<=m; i++)
+        answer = max(answer, store[i]);
+    
     
     return answer;
 }
