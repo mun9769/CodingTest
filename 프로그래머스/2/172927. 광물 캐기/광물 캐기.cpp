05a6f3f7b02e1c4012a101cba5f2d8e407813069
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-04 01:45:55 +0900

    [level 2] Title: 광물 캐기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.cpp
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.cpp
@@ -4,6 +4,8 @@ using namespace std;
 vector<int> picks;
 vector<string> minerals;
 
+int tired[3][3] = {{1,1,1}, {5,1,1}, {25,5,1}};
+
 void filter_picks() {
     int need = (minerals.size()-1) / 5 + 1;
     int inven = accumulate(picks.begin(), picks.end(), 0);
@@ -23,29 +25,35 @@ void filter_picks() {
         picks[0] -= gap;
     }
 }
-int ans = INT32_MAX;
 
-int get_pick(int id) {
-    if(id < picks[0]) return 0;
-    if(id < picks[1] + picks[0]) return 1;
-    if(id < picks[2] + picks[1] + picks[0]) return 2;
+int ans = INT32_MAX;
+int name2num(string name) {
+    if(name == "diamond") return 0;
+    if(name == "iron") return 1;
+    if(name == "stone") return 2;
     return -1;
 }
 
-int calc_tired(int pick, string name) {
-    if(pick == 0) {
-        return 1;
+int calc(int pick, int index) {
+    int sum = 0;
+    for(int i=index; i < minerals.size() and i<index + 5; i++) {
+        int num = name2num(minerals[i]);
+        sum += tired[pick][num];
     }
-    else if(pick == 1) {
-        if(name == "diamond") return 5;
-        return 1;
-    }
-    else if(pick == 2) {
-        if(name == "diamond") return 25;
-        else if(name == "iron") return 5;
-        return 1;
-    }
+    return sum;
 }
+
+int dfs(int a, int b, int c, int index) {
+    if(a + b + c == 0 || index >= minerals.size()) return 0;
+    
+    int ret = INT32_MAX;
+    if(a) ret = min(ret, calc(0, index) + dfs(a-1, b, c, index+5));
+    if(b) ret = min(ret, calc(1, index) + dfs(a, b-1, c, index+5));
+    if(c) ret = min(ret, calc(2, index) + dfs(a, b, c-1, index+5));
+    
+    return ret;
+}
+
 
 int solution(vector<int> _picks, vector<string> _minerals) {
     int answer = 0;
@@ -56,32 +64,8 @@ int solution(vector<int> _picks, vector<string> _minerals) {
     int dia = picks[0];
     int iron = picks[1];
     int stone = picks[2];
-    vector<int> v;
     
-    for(int i=0; i<dia + iron + stone; i++){
-        v.push_back(i);
-    }
-    
-    do {
-        int sum = 0;
-        for(int i=0; i<minerals.size() and i / 5 < dia + iron + stone;) {
-            int id = v[i / 5];
-            int pick = get_pick(id);
-            if(pick == -1) break;
-            
-            int cycle = 5;
-            while(i < minerals.size() and cycle-- > 0) {
-                string name = minerals[i];
-                int tired = calc_tired(pick, name);
-                sum += tired;
-                
-                i++;
-            }
-        }
-        ans = min(ans, sum);
-    } while(next_permutation(v.begin(),v.end()));
-    
-    answer = ans;
+    answer = dfs(dia, iron, stone, 0);
     
     return answer;
 }
