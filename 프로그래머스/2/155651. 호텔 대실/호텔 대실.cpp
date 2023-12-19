ec7af2165608bfaa6250c3613f04418594950a86
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-20 03:18:17 +0900

    [level 2] Title: 호텔 대실, Time: 0.36 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+enum event {
+    release, enter, 
+};
+using task = pair<int, event>;
+
+priority_queue<task, vector<task>, greater<task>> schedule; // TODO: event의 우선순위 정하기
+
+int to_minute(string s) {
+    string hour = s.substr(0, 2);
+    string minute = s.substr(3, 2);
+    
+    int hour_int = stoi(hour);
+    int minute_int = stoi(minute);
+    
+    return hour_int * 60 + minute_int;
+}
+
+int solution(vector<vector<string>> book_time) {
+    int answer = 0;
+    for(auto book: book_time) {
+        int enter_time = to_minute(book[0]);
+        int release_time = to_minute(book[1]);
+        
+        schedule.push({enter_time, event::enter});
+        schedule.push({release_time + 10, event::release});
+    }
+    
+    int using_room = 0;
+    
+    while(schedule.size()) {
+        auto [time, type] = schedule.top();
+        schedule.pop();
+        
+        switch(type) {
+            case event::enter:
+                using_room++;
+                answer = max(answer, using_room);
+                break;
+            case event::release:
+                using_room--;
+                break;
+        }
+    }
+    return answer;
+}
