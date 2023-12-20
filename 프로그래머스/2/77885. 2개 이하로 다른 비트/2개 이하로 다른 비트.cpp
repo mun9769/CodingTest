618ea02af891ff1e8740271a66dcea8eca546b6c
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-20 18:17:41 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.48 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,28 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long diff(long long x) {
-    vector<int> v;
-    
-    while(x > 0) {
-        int remainder = x % 2;
-        x = x / 2;
-        v.push_back(remainder);
-    }
-    v.push_back(0);
-    
-    int one_amount = 0;
-    for(auto bit: v) {
-        if(bit == 0) break;
-        else one_amount++;
-    }
-    
-    if(one_amount == 0)
-        return 1;
-    else 
-        return (long long)pow(2, one_amount-1);
-}
-
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
     
