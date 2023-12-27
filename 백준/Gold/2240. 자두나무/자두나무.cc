898bf84502d088071af87a07ab2d2fe8fc67fb42
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-27 10:55:15 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2400 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,35 +1,39 @@
-// Authored by : jihwan0123
-// Co-authored by : -
-// http://boj.kr/8d19497b3eb04988bbacfff15129c98d
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int a[1002];
-int d[1002][32][3]; // d[i][j][k]: i:현재시간, j:이동횟수, k:나무번호
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int t, w;
-  cin >> t >> w;
-  for (int i = 1; i <= t; i++)
-    cin >> a[i];
+using ii = pair<int, int>;
+using ll = long long;
 
-  for (int i = 1; i <= t; i++) {
-    // 이동하지 않는 경우 (2번 나무는 이동안하면 못감)
-    d[i][0][1] = d[i - 1][0][1] + (a[i] == 1 ? 1 : 0);
-    for (int j = 1; j <= w; j++) { // j번 이동
-      if (i < j) break; // 현재 시간보다 많이 이동할 수 없다
-      if (a[i] == 1) { // 1번 나무인 경우
-        d[i][j][1] = max(d[i - 1][j - 1][2], d[i - 1][j][1]) + 1;
-        d[i][j][2] = max(d[i - 1][j - 1][1], d[i - 1][j][2]);
-      }
-      else { // 2번 나무인 경우
-        d[i][j][1] = max(d[i - 1][j - 1][2], d[i - 1][j][1]);
-        d[i][j][2] = max(d[i - 1][j - 1][1], d[i - 1][j][2]) + 1;
-      }
+int t, max_w;
+int dp[1002][32][3];
+int zadu[1002];
+
+int main() {
+    cin >> t >> max_w;
+    for(int i=1; i<=t; i++) cin >> zadu[i];
+
+    for(int i=1; i<=t; i++) {
+
+        dp[i][0][1] = dp[i-1][0][1] + (zadu[i] == 1 ? 1 : 0);
+
+        for(int w=1; w<=max_w; w++) {
+            if(i < w) break;
+            if(zadu[i] == 1) {
+                dp[i][w][1] = max(dp[i - 1][w][1], dp[i - 1][w - 1][2]) + 1;
+                dp[i][w][2] = max(dp[i - 1][w][2], dp[i - 1][w - 1][1]);
+            }
+            else{
+                dp[i][w][1] = max(dp[i - 1][w][1], dp[i - 1][w - 1][2]);
+                dp[i][w][2] = max(dp[i - 1][w][2], dp[i - 1][w - 1][1]) + 1;
+            }
+
+        }
     }
-  }
-  int ans = 0;
-  for (int j = 0; j <= w; j++)
-    ans = max({ans, d[t][j][1], d[t][j][2]});
-  cout << ans;
+
+    int answer = 0;
+    for(int w=0; w<=max_w; w++) {
+        int mx = max(dp[t][w][1], dp[t][w][2]);
+        answer = max(answer, mx);
+    }
+    cout << answer;
 }
