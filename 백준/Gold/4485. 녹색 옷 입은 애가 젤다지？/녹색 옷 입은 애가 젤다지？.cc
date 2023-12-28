72f7ff1cc97bb9395ad99048b5e32795918f2437
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-29 01:29:00 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -7,58 +7,13 @@ using ll = long long;
 #define inf 987654321
 
 
-int n, m;
+int n;
 
 int board[130][130];
-vector<iii> adj[130][130];
 int dist[130][130];
 
-int accumulate() {
-    for(int i=1; i<n; i++) { // 상단
-        for(int j=0; j<i+1; j++) {
-
-            int x = i - j + 1;
-            int y = j + 1;
-
-            int top = board[x-1][y];
-            int lft = board[x][y-1];
-
-            board[x][y] += min(top, lft);
-        }
-    }
-
-    for(int i=n; i<2*n-1; i++) {
-        for(int j=n-1; j>=1; j--) {
-
-            int y = i - j + 1;
-            int x = j + 1;
-
-            int top = board[x-1][y];
-            int lft = board[x][y-1];
-
-            board[x][y] += min(top, lft);
-        }
-    }
-
-    return board[n][n];
-}
-
-void make_adjacent() {
-    vector<ii> offsets = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, };
-
-    for(int i=1; i<=n; i++) for(int j=1; j<=n; j++) {
-
-        for(ii offset: offsets) {
-            int nx = i + offset.first;
-            int ny = j + offset.second;
-            if(nx < 1 || ny < 1 || nx > n || ny > n) continue;
-
-            adj[i][j].push_back({board[nx][ny], nx, ny});
-        }
-    }
-}
-
 int dijkstra() {
+    vector<ii> offsets = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, };
     priority_queue<iii, vector<iii>, greater<iii>> pq;
     pq.push({board[1][1], 1, 1});
     dist[1][1] = board[1][1];
@@ -68,7 +23,12 @@ int dijkstra() {
         pq.pop();
         if(x == n and y == n) return acc;
 
-        for(auto [cost, nx, ny]: adj[x][y]) {
+        for(ii offset: offsets) {
+            int nx = x + offset.first;
+            int ny = y + offset.second;
+            if(nx < 1 || ny < 1 || nx > n || ny > n) continue;
+            int cost = board[nx][ny];
+
             if(dist[nx][ny] > acc + cost) {
                 dist[nx][ny] = acc + cost;
                 pq.push({dist[nx][ny], nx, ny});
@@ -84,10 +44,8 @@ void init() {
             cin >> board[i][j];
     }
     for(int i=1; i<=n; i++) for(int j=1; j<=n; j++) {
-            adj[i][j].clear();
             dist[i][j] = inf;
     }
-    make_adjacent();
 }
 
 int main() {
