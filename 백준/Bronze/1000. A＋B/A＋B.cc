c71d2fd2560acd48c32c910c22bedcf8348887fa
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-30 17:18:48 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 3116 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,10 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+using ii = pair<int, int>;
+using iii = tuple<int, int, int>;
+using ll = long long;
+#define inf 987654321
 
-int main() {
-	int a=1, b = 2;
-	cin >> a; //cin>>A,B;는 안되나
-	cin >> b;
-	cout << a + b << endl;
+int n;
+int arr[20'005];
+int cnt[20'005];
+vector<ii> two[40'005];
+ll answer;
 
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
 }
