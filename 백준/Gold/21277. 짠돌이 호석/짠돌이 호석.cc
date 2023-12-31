89907c441f558167c6cd8f03adacd29190aa1c77
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-31 20:32:10 +0900

    [Gold III] Title: 짠돌이 호석, Time: 24 ms, Memory: 2228 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.cc
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.cc
@@ -10,109 +10,71 @@ using ll = long long;
 int n1, m1, n2, m2;
 int b1[200][200];
 int b2[103][103];
-int answer;
 
-void count_func(ii st) {
-    bool possible[8] = { true, true, true, true, true, true, true, true, };
+int rot[52][52];
+int answer = INT32_MAX;
 
-    for(int i = 0; i<n2; i++) {
+void rotate() {
+    for(int i=0; i<n2; i++) {
         for(int j=0; j<m2; j++) {
-            if(b1[st.first + i][st.second + j] and b2[i][j]) { possible[0] = false; }
-
-            // if(b1[st.first + i][st.second - j] and b2[i][j]) { possible[1] = false; }
-
-            if(b1[st.first - j][st.second + i] and b2[i][j]) { possible[2] = false; }
-
-            // if(b1[st.first + j][st.second + i] and b2[i][j]) { possible[3] = false; }
-
-            if(b1[st.first - i][st.second - j] and b2[i][j]) { possible[4] = false; }
-
-            // if(b1[st.first - i][st.second + j] and b2[i][j]) { possible[5] = false; }
-
-            if(b1[st.first + j][st.second - i] and b2[i][j]) { possible[6] = false; }
-
-//            if(b1[st.first - j][st.second - i] and b2[i][j]) { possible[7] = false; }
+            rot[m2-1-j][i] = b2[i][j];
         }
     }
 
-    st.first -= off;
-    st.second -= off;
-
-    int area = INT32_MAX;
-    int width;
-    int height;
-
-    if(possible[0]) {
-        width  = max(m1, m2 + st.second);
-        height = max(n1, n2 + st.first);
-        area = min(area, width * height);
+    swap(n2 ,m2);
+    for(int i=0; i<n2; i++) {
+        for(int j=0; j<m2; j++) {
+            b2[i][j] = rot[i][j];
+            // out bound에 더티값이 저장되어 있음을 유의하라.
+        }
     }
-//    if(possible[1]) {
-//        width  = max(m2, m1 + m2 - st.second - 1);
-//        height = max(n1, n2 + st.first);
-//        area = min(area, width * height);
-//    }
+}
 
-    if(possible[2]) {
-        width  = max(m1, n2 + st.second);
-        height = max(n1, n1 + m2 - st.first - 1);
-        area = min(area, width * height);
+void solve(int x, int y) {
+
+    for(int i=x; i< x + n2; i++) {
+        for(int j=y; j<y+m2; j++) {
+            if(b2[i-x][j-y] and b1[i][j]) {
+                return;
+            }
+        }
     }
-//    if(possible[3]) {
-//        width  = max(m1, n2 + st.second);
-//        height = max(n1, m2 + st.first);
-//        area = min(area, width * height);
-//    }
 
-    if(possible[4]) {
-        width  = max(m1, m1+m2 - st.second -1);
-        height = max(n1, n1+n2 - st.first - 1);
-        area = min(area, width * height);
-    }
-//    if(possible[5]) {
-//        width  = max(m1, m2 + st.second);
-//        height = max(n1, n1+n2 - st.first - 1);
-//        area = min(area, width * height);
-//    }
+    int min_x = min(x, 50);
+    int max_x = max(50 + n1, x + n2);
 
-    if(possible[6]) {
-        width  = max(m1, m1+n2 - st.second - 1);
-        height = max(n1, m2 + st.first);
-        area = min(area, width * height);
-    }
-//    if(possible[7]) {
-//        width  = max(m1, m1+n2 - st.second - 1);
-//        height = max(n1, n1+m2 - st.first - 1);
-//        area = min(area, width * height);
-//    }
+    int min_y = min(y, 50);
+    int max_y = max(50 + m1, y + m2);
 
-    answer = min(area, answer);
+    int area = (max_x - min_x) * (max_y - min_y);
+    answer = min(answer, area);
 }
 
 int main()
 {
     cin >> n1 >> m1;
     for(int i=0; i<n1; i++) {
-        string s; cin >> s;
         for(int j=0; j<m1; j++) {
-            b1[i+off][j+off] = s[j] - '0';
+            char c; cin >> c;
+            b1[i+off][j+off] = c-'0';
         }
     }
     cin >> n2 >> m2;
     for(int i=0; i<n2; i++) {
-        string s; cin >> s;
         for(int j=0; j<m2; j++) {
-            b2[i][j] = s[j] - '0';
+            char c; cin >> c;
+            b2[i][j] = c-'0';
         }
     }
-    int ans1 = (n1+n2) * max(m1, m2);
-    int ans2 = (m1+m2) * max(n1, n2);
-    answer = min(ans1, ans2);
 
-    for(int i=off; i<off+n1; i++) for(int j=off; j<off+m1; j++) {
-        count_func({i, j});
+    for(int k=0; k<4; k++) {
+        rotate();
+        for(int i=0; i<off+n1; i++) {
+            for(int j=0; j<off+m1; j++) {
+                solve(i, j);
+            }
+        }
     }
-
     cout << answer;
 
 }
