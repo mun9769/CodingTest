d6cf40ad578a94146f99aecc85711c18aad87343
Moon <59304977+mun9769@users.noreply.github.com>
2024-01-05 22:47:21 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ii = pair<int, int>;
+using iii = tuple<int, int, int>;
+using ll = long long;
+#define inf 987654321
+#define Mxn 20'001
+
+
+
+int main() {
+    cin.tie(0) ->sync_with_stdio(0);
+    int a, b;
+    cin >> a >> b;
+
+    cout << 2 * b - a;
+
+}
