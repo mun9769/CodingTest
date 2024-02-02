8c1635d12bde48dca1583695dc4336d5f60ce15d
Moon <59304977+mun9769@users.noreply.github.com>
2024-02-02 15:11:45 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 596 ms, Memory: 209864 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,26 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+using ll = long long;
+
+string str;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> str;
+    unordered_set<string> us;
+    for (int i = 0; i < str.size(); i++) {
+
+        for (int sz = 1; i + sz <= str.size(); sz++) {
+            string sub = str.substr(i, sz);
+            us.insert(sub);
+        }
+    }
+    cout << us.size();
+
+    return 0;
+}
