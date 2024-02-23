59e6a0bcfd9621c131d4aa408ce31ee8d110b712
Moon <59304977+mun9769@users.noreply.github.com>
2024-02-23 13:41:59 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 632 ms, Memory: 8132 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ii = pair<int, int>;
+int n, m;
+vector<int> adj[1004];
+bool vis[1004];
+stack<int> s;
+
+int main()
+{
+    cin >> n >> m;
+    for(int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    int ans = 0;
+
+    for(int i=1; i<=n; i++) {
+        if(vis[i] == false) {
+            s.push(i);
+            ans++;
+        }
+
+        while(s.size()) {
+            int cur = s.top();
+            s.pop();
+            vis[cur] = true;
+
+            for(auto nxt: adj[cur]) {
+                if(vis[nxt] == false) {
+                    s.push(nxt);
+                }
+            }
+        }
+
+    }
+    cout << ans;
+
+
+}
