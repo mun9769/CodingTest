ddf475a3457fbc2c38d23aa49011543a21b0a824
Moon <59304977+mun9769@users.noreply.github.com>
2024-02-24 00:46:54 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 4 ms, Memory: 2608 KB -BaekjoonHub

---
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.cc
@@ -0,0 +1,72 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int arr[50'004];
+
+int node[50'005];
+int edge[50'005];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n >> k;
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    int p = 0;
+    int i = 0;
+    for(i=0; i<n; i++) {
+        if(arr[i] % 2 == 0)
+            break;
+    }
+
+    for(; i<n; ) {
+        int cur = i;
+        bool e = (arr[cur] % 2 == 0) ? true : false;
+        if(e) {
+            while(cur < n and arr[cur] % 2 == 0)
+                cur++;
+            node[p] = cur - i;
+        }
+        else {
+            while(cur < n and arr[cur] % 2 == 1)
+                cur++;
+            edge[p++] = cur - i;
+        }
+        i = cur;
+    }
+
+    for(i=0; i<50'004 and node[i] != 0; i++){
+    }
+    int sz = i;
+    int ans = node[0];
+    int cnt = node[0];
+    int cost = 0;
+
+    int a = 0, b = 1;
+    for(;b < sz;) {
+        if(cost + edge[b-1] <= k) {
+            cost += edge[b-1];
+            cnt += node[b];
+            b++;
+        }
+        else {
+            if(a+1 == b) {
+                a++; b++;
+                cnt = node[a];
+            }
+            else {
+                cnt -= node[a];
+                cost -= edge[a];
+                a++;
+            }
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans;
+
+}
